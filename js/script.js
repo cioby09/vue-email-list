75686801fbb87471a1225ed2c0424745427a1f23
cioby09 <claudiociobanu09@gmail.com>
2023-04-19 20:59:39 +0200

    Feat: generate random emails with API

--- js/script.js
+++ js/script.js
@@ -9,4 +9,19 @@ const {createApp} = Vue;
 
 createApp({
 
+    data() {
+        return {
+            randomEmail: []
+        }
+    },
+    mounted() {
+        for (i=0; i<10; i++) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp)=> {
+                    console.log(resp.data.response);
+                    this.randomEmail.push(resp.data.response)
+                })
+        }
+    }
 }).mount("#app");
